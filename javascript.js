0d034f20cf60992f27a64be94b2ad0898b87f955
codie valkhof <40520@ma-web.nl>
2026-04-18 15:13:07 +0200

    s

--- javascript.js
+++ javascript.js
@@ -1,4 +1,31 @@
 function scrollToSection(section) {
     const element = document.querySelector(section);
-    element.scrollIntoView({behavior: "smooth"});
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function myFunction() {
+    let input = document.getElementById("myinput");
+    let filter = input.value.toLowerCase();
+    let products = document.getElementsByClassName("product");
+    let visibleCount = 0;
+
+    for (let i = 0; i < products.length; i++) {
+        let name = products[i].getElementsByClassName("product-name")[0];
+        let textValue = name.textContent || name.innerText;
+
+        if (textValue.toLowerCase().indexOf(filter) > -1) {
+            products[i].style.display = "block";
+            visibleCount++;
+        } else {
+            products[i].style.display = "none";
+        }
     }
+
+    let noResults = document.getElementById("no-results");
+
+    if (visibleCount === 0) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
+    }
+}
